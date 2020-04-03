57f7effb6fad9b2713fa72785df2cd30f339c75e
Abhijith Ajith Kumar <abheethy@gmail.com>
2020-04-03 19:12:18 +0530

    map added

--- sample.js
+++ sample.js
@@ -26,6 +26,18 @@ function myFunction(event) {
 		window.open('https://www.linkedin.com/in/abhijithajithkumar/', '_blank')
 	}
 }
+var map = new ol.Map({
+    target: 'map',
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([80.2277203, 12.9117073]),
+      zoom: 15	
+    })
+  });
 function downlodResume(e){
 	e.preventDefault();  
 	var URL = 'https://drive.google.com/file/d/1ezFPrxcgMnqE8zgbPxvdPS4dbCYa1-C5/view?usp=sharing'
