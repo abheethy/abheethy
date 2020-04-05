8bf2db00516e1dfbdbeaff4bdf5db72a6c6b1751
Abhijith Ajith Kumar <abheethy@gmail.com>
2020-04-05 16:06:24 +0530

    commit

--- sample.js
+++ sample.js
@@ -1,6 +1,52 @@
+document.getElementsByClassName('abheethy-bar-item')[0].style.backgroundColor = '#000';
 var fbElement= document.getElementById('fb_icon');
 var instaElement= document.getElementById('insta_icon');
 var linkdnElement= document.getElementById('linkdn_icon');
+var navElements = document.getElementsByClassName('abheethy-bar-item');
+for (var i = 0; i < navElements.length; i++) {
+	navElements[i].addEventListener('click', event => {
+		navigateBar(event);
+});
+}
+function navigateBar(event) {
+	var id  =  event.currentTarget.id;
+	if(event.currentTarget.textContent.includes("ABOUT")){
+		document.getElementsByClassName('abheethy-bar-item')[0].style.backgroundColor = '#222';
+		document.getElementsByClassName('abheethy-bar-item')[1].style.backgroundColor = '#000';
+		document.getElementsByClassName('abheethy-bar-item')[2].style.backgroundColor = '#222';
+		document.getElementsByClassName('abheethy-bar-item')[3].style.backgroundColor = '#222';
+	}else if(event.currentTarget.textContent.includes("HOME")){
+		document.getElementsByClassName('abheethy-bar-item')[0].style.backgroundColor = '#000';
+		document.getElementsByClassName('abheethy-bar-item')[1].style.backgroundColor = '#222';
+		document.getElementsByClassName('abheethy-bar-item')[2].style.backgroundColor = '#222';
+		document.getElementsByClassName('abheethy-bar-item')[3].style.backgroundColor = '#222';
+	}else if(event.currentTarget.textContent.includes("PHOTOS")){
+		document.getElementsByClassName('abheethy-bar-item')[0].style.backgroundColor = '#222';
+		document.getElementsByClassName('abheethy-bar-item')[1].style.backgroundColor = '#222';
+		document.getElementsByClassName('abheethy-bar-item')[2].style.backgroundColor = '#000';
+		document.getElementsByClassName('abheethy-bar-item')[3].style.backgroundColor = '#222';
+	}else{
+		document.getElementsByClassName('abheethy-bar-item')[0].style.backgroundColor = '#222';
+		document.getElementsByClassName('abheethy-bar-item')[1].style.backgroundColor = '#222';
+		document.getElementsByClassName('abheethy-bar-item')[2].style.backgroundColor = '#222';
+		document.getElementsByClassName('abheethy-bar-item')[3].style.backgroundColor = '#000';
+	}
+	
+	var progressOne = document.getElementsByClassName('progress-one')[0];
+	var progressTwo = document.getElementsByClassName('progress-two')[0];
+	var progressThree = document.getElementsByClassName('progress-three')[0];
+	var progressFour = document.getElementsByClassName('progress-four')[0];
+	progressOne.classList.remove('progressone');
+	progressTwo.classList.remove('progresstwo');
+	progressThree.classList.remove('progressthree');
+	progressFour.classList.remove('progressfour');
+	if(event.currentTarget.textContent.includes("ABOUT")){
+		progressOne.classList.add('progressone');
+		progressTwo.classList.add('progresstwo');
+		progressThree.classList.add('progressthree');
+		progressFour.classList.add('progressfour');
+	}
+}
 
 var downloadResume= document.getElementById('download-resume');
 linkdnElement.addEventListener('click', event => {
